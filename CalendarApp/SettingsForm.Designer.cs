7882cff89e7c2ec76eca58d93796d9f746c1f1d4
Nikita <nikita.dmitrovich@gmail.com>
2021-07-08 10:44:07 +0300

    Add localization on Settings Form

--- CalendarApp/SettingsForm.Designer.cs
+++ CalendarApp/SettingsForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SettingsForm));
             this.LangButton = new System.Windows.Forms.Button();
             this.FolderButton = new System.Windows.Forms.Button();
             this.BGColor = new System.Windows.Forms.Button();
@@ -38,66 +39,41 @@
             // 
             // LangButton
             // 
-            this.LangButton.Location = new System.Drawing.Point(252, 46);
+            resources.ApplyResources(this.LangButton, "LangButton");
             this.LangButton.Name = "LangButton";
-            this.LangButton.Size = new System.Drawing.Size(75, 23);
-            this.LangButton.TabIndex = 0;
-            this.LangButton.Text = "Language";
             this.LangButton.UseVisualStyleBackColor = true;
             // 
             // FolderButton
             // 
-            this.FolderButton.Location = new System.Drawing.Point(252, 78);
+            resources.ApplyResources(this.FolderButton, "FolderButton");
             this.FolderButton.Name = "FolderButton";
-            this.FolderButton.Size = new System.Drawing.Size(75, 23);
-            this.FolderButton.TabIndex = 1;
-            this.FolderButton.Text = "Folder";
             this.FolderButton.UseVisualStyleBackColor = true;
             // 
             // BGColor
             // 
-            this.BGColor.Location = new System.Drawing.Point(252, 110);
+            resources.ApplyResources(this.BGColor, "BGColor");
             this.BGColor.Name = "BGColor";
-            this.BGColor.Size = new System.Drawing.Size(75, 23);
-            this.BGColor.TabIndex = 2;
-            this.BGColor.Text = "Color";
             this.BGColor.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(21, 43);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(95, 24);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Language";
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(21, 78);
+            resources.ApplyResources(this.label2, "label2");
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(112, 24);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Save Folder";
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(21, 110);
+            resources.ApplyResources(this.label3, "label3");
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(165, 24);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "BackGround Color";
             // 
             // SettingsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(339, 172);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -106,7 +82,6 @@
             this.Controls.Add(this.LangButton);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "SettingsForm";
-            this.Text = "Settings";
             this.ResumeLayout(false);
             this.PerformLayout();
 

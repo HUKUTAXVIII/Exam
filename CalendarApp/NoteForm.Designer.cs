4ba5ee36d00af328f64d6f37f8779bd77c7af838
Nikita <nikita.dmitrovich@gmail.com>
2021-07-08 10:51:08 +0300

    Add localization on Note Form

--- CalendarApp/NoteForm.Designer.cs
+++ CalendarApp/NoteForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NoteForm));
             this.NameBox = new System.Windows.Forms.TextBox();
             this.ContentBox = new System.Windows.Forms.TextBox();
             this.TimeBox = new System.Windows.Forms.Label();
@@ -35,40 +36,28 @@
             // 
             // NameBox
             // 
-            this.NameBox.Location = new System.Drawing.Point(12, 12);
+            resources.ApplyResources(this.NameBox, "NameBox");
             this.NameBox.Name = "NameBox";
-            this.NameBox.Size = new System.Drawing.Size(307, 20);
-            this.NameBox.TabIndex = 0;
             // 
             // ContentBox
             // 
-            this.ContentBox.Location = new System.Drawing.Point(12, 38);
-            this.ContentBox.Multiline = true;
+            resources.ApplyResources(this.ContentBox, "ContentBox");
             this.ContentBox.Name = "ContentBox";
-            this.ContentBox.Size = new System.Drawing.Size(307, 422);
-            this.ContentBox.TabIndex = 1;
             // 
             // TimeBox
             // 
-            this.TimeBox.AutoSize = true;
-            this.TimeBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.TimeBox.Location = new System.Drawing.Point(9, 463);
+            resources.ApplyResources(this.TimeBox, "TimeBox");
             this.TimeBox.Name = "TimeBox";
-            this.TimeBox.Size = new System.Drawing.Size(100, 24);
-            this.TimeBox.TabIndex = 2;
-            this.TimeBox.Text = "08.07.2021";
             // 
             // NoteForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(331, 505);
             this.Controls.Add(this.TimeBox);
             this.Controls.Add(this.ContentBox);
             this.Controls.Add(this.NameBox);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "NoteForm";
-            this.Text = "NoteForm";
             this.ResumeLayout(false);
             this.PerformLayout();
 

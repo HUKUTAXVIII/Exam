811fb77a0d4d61460ec76f4cab1c1458c778645a
Nikita <nikita.dmitrovich@gmail.com>
2021-07-08 09:37:14 +0300

    AddGUI

--- CalendarApp/Form1.Designer.cs
+++ CalendarApp/Form1.Designer.cs
@@ -28,30 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.Calendar = new System.Windows.Forms.MonthCalendar();
+            this.NotePanel = new System.Windows.Forms.Panel();
+            this.SearchBox = new System.Windows.Forms.TextBox();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.SettingsButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // monthCalendar1
+            // Calendar
             // 
-            this.monthCalendar1.Location = new System.Drawing.Point(75, 71);
-            this.monthCalendar1.Name = "monthCalendar1";
-            this.monthCalendar1.TabIndex = 0;
+            this.Calendar.Location = new System.Drawing.Point(82, 55);
+            this.Calendar.Name = "Calendar";
+            this.Calendar.TabIndex = 0;
+            // 
+            // NotePanel
+            // 
+            this.NotePanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.NotePanel.Location = new System.Drawing.Point(350, 22);
+            this.NotePanel.Name = "NotePanel";
+            this.NotePanel.Size = new System.Drawing.Size(200, 270);
+            this.NotePanel.TabIndex = 1;
+            // 
+            // SearchBox
+            // 
+            this.SearchBox.Location = new System.Drawing.Point(12, 305);
+            this.SearchBox.Name = "SearchBox";
+            this.SearchBox.Size = new System.Drawing.Size(252, 20);
+            this.SearchBox.TabIndex = 2;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(270, 305);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(74, 23);
+            this.SearchButton.TabIndex = 3;
+            this.SearchButton.Text = "🔍";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(350, 305);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(74, 23);
+            this.AddButton.TabIndex = 4;
+            this.AddButton.Text = "+";
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(430, 305);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(74, 23);
+            this.DeleteButton.TabIndex = 5;
+            this.DeleteButton.Text = "-";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            // 
+            // SettingsButton
+            // 
+            this.SettingsButton.Location = new System.Drawing.Point(528, 305);
+            this.SettingsButton.Name = "SettingsButton";
+            this.SettingsButton.Size = new System.Drawing.Size(22, 23);
+            this.SettingsButton.TabIndex = 6;
+            this.SettingsButton.Text = "⚙";
+            this.SettingsButton.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.monthCalendar1);
+            this.ClientSize = new System.Drawing.Size(567, 337);
+            this.Controls.Add(this.SettingsButton);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.SearchBox);
+            this.Controls.Add(this.NotePanel);
+            this.Controls.Add(this.Calendar);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.MonthCalendar Calendar;
+        private System.Windows.Forms.Panel NotePanel;
+        private System.Windows.Forms.TextBox SearchBox;
+        private System.Windows.Forms.Button SearchButton;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button SettingsButton;
     }
 }
 

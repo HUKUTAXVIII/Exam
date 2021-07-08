b054dc400d68b5c036b7609a7be4915ededd7dd2
Nikita <nikita.dmitrovich@gmail.com>
2021-07-08 10:48:27 +0300

    Add Note Form

---
+++ CalendarApp/NoteForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace CalendarApp
+{
+    partial class NoteForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.ContentBox = new System.Windows.Forms.TextBox();
+            this.TimeBox = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(12, 12);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(307, 20);
+            this.NameBox.TabIndex = 0;
+            // 
+            // ContentBox
+            // 
+            this.ContentBox.Location = new System.Drawing.Point(12, 38);
+            this.ContentBox.Multiline = true;
+            this.ContentBox.Name = "ContentBox";
+            this.ContentBox.Size = new System.Drawing.Size(307, 422);
+            this.ContentBox.TabIndex = 1;
+            // 
+            // TimeBox
+            // 
+            this.TimeBox.AutoSize = true;
+            this.TimeBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TimeBox.Location = new System.Drawing.Point(9, 463);
+            this.TimeBox.Name = "TimeBox";
+            this.TimeBox.Size = new System.Drawing.Size(100, 24);
+            this.TimeBox.TabIndex = 2;
+            this.TimeBox.Text = "08.07.2021";
+            // 
+            // NoteForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(331, 505);
+            this.Controls.Add(this.TimeBox);
+            this.Controls.Add(this.ContentBox);
+            this.Controls.Add(this.NameBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "NoteForm";
+            this.Text = "NoteForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.TextBox ContentBox;
+        private System.Windows.Forms.Label TimeBox;
+    }
+}

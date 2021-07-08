10d6df247c5ee5e26a97225dd91caf02d0ef1453
Nikita <nikita.dmitrovich@gmail.com>
2021-07-08 10:59:12 +0300

    Add Folder Existing Check

--- NoteLib/NoteManager.cs
+++ NoteLib/NoteManager.cs
@@ -17,31 +17,46 @@ namespace NoteLib
         }
 
         public bool LoadNotes(string Path) {
-            bool isCorrect = false;
-
-            Directory.GetDirectories(Path + "/Notes/").ToList().ForEach((date)=> {
-                Directory.GetFiles(date).ToList().ForEach((file)=> {
-                    Notes.Add(new Note());
-                    Notes.Last().Name = new FileInfo(file).Name.Substring(0, new FileInfo(file).Name.Length-4);
-                    Notes.Last().Content = File.ReadAllText(file);
-                    Notes.Last().CalendarPosition = Convert.ToDateTime(new DirectoryInfo(date).Name);
+            bool isCorrect = true;
+            if (Directory.Exists(Path + "/Notes/"))
+            {
+                Directory.GetDirectories(Path + "/Notes/").ToList().ForEach((date) =>
+                {
+                    Directory.GetFiles(date).ToList().ForEach((file) =>
+                    {
+                        Notes.Add(new Note());
+                        Notes.Last().Name = new FileInfo(file).Name.Substring(0, new FileInfo(file).Name.Length - 4);
+                        Notes.Last().Content = File.ReadAllText(file);
+                        Notes.Last().CalendarPosition = Convert.ToDateTime(new DirectoryInfo(date).Name);
+                    });
                 });
-            });
-
+            }
+            else {
+                isCorrect = false;
+            }
             return isCorrect;
         }
         public bool SaveNotes(string Path)
         {
-            bool isCorrect = false;
+            bool isCorrect = true;
 
-            Directory.CreateDirectory("Notes");
-            this.Notes.ForEach((note)=> {
+            if (!Directory.Exists(Path))
+            {
+                Directory.CreateDirectory(Path);
+            }
+            if (!Directory.Exists(Path + "Notes"))
+            {
+                Directory.CreateDirectory(Path + "Notes");
+            }
+            this.Notes.ForEach((note) =>
+            {
                 Directory.CreateDirectory(note.CalendarPosition.ToShortDateString());
-                File.Create(Path+"/Notes/" + note.CalendarPosition.ToShortDateString()+"/"+note.Name).Close();
-                File.WriteAllText(Path + "/Notes/" + note.CalendarPosition.ToShortDateString() + "/" + note.Name,note.Content);
+                File.Create(Path + "/Notes/" + note.CalendarPosition.ToShortDateString() + "/" + note.Name).Close();
+                File.WriteAllText(Path + "/Notes/" + note.CalendarPosition.ToShortDateString() + "/" + note.Name, note.Content);
             });
 
             return isCorrect;
+        
         }
 
     }

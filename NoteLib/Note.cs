a3252836300788b31cfe34e0952ee27e405fc501
Nikita <nikita.dmitrovich@gmail.com>
2021-07-08 09:48:56 +0300

    Add Note Class

--- NoteLib/Note.cs
+++ NoteLib/Note.cs
@@ -8,5 +8,25 @@ namespace NoteLib
 {
     public class Note
     {
+        public string Name;
+        public string Content;
+        public DateTime CalendarPosition;
+
+        public Note()
+        {
+
+        }
+        public Note(string name,string content,DateTime pos)
+        {
+            Name = name;
+            Content = content;
+            CalendarPosition = pos;
+        }
+
+
+
+
+
+
     }
 }
